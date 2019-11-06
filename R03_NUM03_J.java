d96d1df16c47091caf6f47e2373f9fd0227a2e3b
daucf23 <davidalmeida2@knights.ucf.edu>
2019-11-06 00:27:55 -0500

    clear

--- R03_NUM03_J.java
+++
@@ -1,8 +0,0 @@
-public static int getInteger(DataInputStream is) throws IOException {
-  return is.readInt(); 
-}
-
-
-public static long getInteger(DataInputStream is) throws IOException {
-  return is.readInt() & 0xFFFFFFFFL; // Mask with 32 one-bits
-}
--- README.md
+++
@@ -1,3 +0,0 @@
-# HW2DavidAlmeida
------------------------
-This contains the 10 small java examples of insecure code to be tested in SNYK
