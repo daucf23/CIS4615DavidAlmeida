c633401dea17520b656c7b661c1abd0452729315
daucf23 <davidalmeida2@knights.ucf.edu>
2019-11-06 00:17:20 -0500

    pushing latest updates, STR01 and IDS03 incomplete

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -1,30 +1,33 @@
-public class ROO_IDS03_J 
-{
+import java.util.logging.*;
+import 
+public class R00_IDS03_J {
 	
 	public static void main(String[] args)
 	{
-
-		if(loginSuccessful)
-		{
-			logger.severe("User login succeeded for: " + username);
-		} else {
-			logger.severe("User login failed for: " + username);
-		}
-
-
-
-
-		if(loginSuccessful){
-			logger.severe("USer login succeeded for: " + sanatizeUser(username));
-		} else {
-			logger.severe("User login failed for: " + sanatizeUser(username));
-		}
-		public String sanitizeUser(String username) {
-  			return Pattern.matches("[A-Za-z0-9_]+", username))
-     			 ? username : "unauthorized user";
-}
-
-
+		System.out.println("User login");
+		
+		
+		
 	}
+	
+	//improper implementation
+	if (loginSuccessful) {
+		  logger.severe("User login succeeded for: " + username);
+		} else {
+		  logger.severe("User login failed for: " + username);
+		}
+	
+	
+	//proper implementation with sanatize user function
+	if (loginSuccessful) {
+		  logger.severe("User login succeeded for: " + sanitizeUser(username));
+		} else {
+		  logger.severe("User login failed for: " + sanitizeUser(username));
+		}
+	public String sanitizeUser(String username) {
+		  return Pattern.matches("[A-Za-z0-9_]+", username))
+		      ? username : "unauthorized user";
+		}
+	
 
 }
